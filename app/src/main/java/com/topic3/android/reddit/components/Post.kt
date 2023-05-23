0822be9f845d7e8cd9b8d330aa5ea00b19c420a4
wickedside <w1ck3ds1d3@gmail.com>
2023-05-23 23:23:40 +0300

    Changed function Header

--- app/src/main/java/com/topic3/android/reddit/components/Post.kt
+++ app/src/main/java/com/topic3/android/reddit/components/Post.kt
@@ -60,8 +60,14 @@ fun Post(post: PostModel, content: @Composable () -> Unit = {}) {
 }
 
 @Composable
-fun Header(post: PostModel) {
-    Row(modifier = Modifier.padding(start = 16.dp)) {
+fun Header(
+    post: PostModel,
+    onJoinButtonClick: (Boolean) -> Unit = {}
+) {
+    Row(
+        modifier = Modifier.padding(start = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Image(
             ImageBitmap.imageResource(id = R.drawable.subreddit_placeholder),
             contentDescription = stringResource(id = R.string.subreddits),
@@ -81,6 +87,8 @@ fun Header(post: PostModel) {
                 color = Color.Gray
             )
         }
+        Spacer(modifier = Modifier.width(4.dp))
+        JoinButton(onJoinButtonClick)
         MoreActionsMenu()
     }
     Title(text = post.title)
