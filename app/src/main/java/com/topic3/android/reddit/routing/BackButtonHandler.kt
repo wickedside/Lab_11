400f05c84d8d84b9d7c60b0bdea649f665ecc146
wickedside <w1ck3ds1d3@gmail.com>
2023-05-23 23:00:50 +0300

    Changed file BackButtonHandler.kt

--- app/src/main/java/com/topic3/android/reddit/routing/BackButtonHandler.kt
+++ app/src/main/java/com/topic3/android/reddit/routing/BackButtonHandler.kt
@@ -1,11 +1,14 @@
 package com.topic3.android.reddit.routing
 
+import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.OnBackPressedDispatcher
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalLifecycleOwner
 
 private val localBackPressedDispatcher =
     staticCompositionLocalOf<OnBackPressedDispatcher?> { null }
@@ -25,5 +28,17 @@ fun BackButtonHandler(
     DisposableEffect(dispatcher) {
         dispatcher.addCallback(backCallback)
         onDispose { backCallback.remove() }
+    }
+}
+
+@Composable
+fun BackButtonAction(onBackPressed: () -> Unit)
+{
+    CompositionLocalProvider (localBackPressedDispatcher provides (
+            LocalLifecycleOwner.current as ComponentActivity
+            ).onBackPressedDispatcher){
+        BackButtonHandler {
+            onBackPressed.invoke()
+        }
     }
 }
