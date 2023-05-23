763fd3e549431e75575b95dfa5f96de7553b4e05
wickedside <w1ck3ds1d3@gmail.com>
2023-05-23 23:33:33 +0300

    Completed file JoinButton.kt

--- app/src/main/java/com/topic3/android/reddit/components/JoinButton.kt
+++ app/src/main/java/com/topic3/android/reddit/components/JoinButton.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
@@ -29,6 +32,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun JoinButton(onClick: (Boolean) -> Unit = {}){
@@ -98,7 +102,7 @@ fun JoinButton(onClick: (Boolean) -> Unit = {}){
             .border(width = 1.dp, color = Color.Blue, shape = shape)
             .background(color = buttonBackgroundColor)
             .size(
-                width = 40.dp,
+                width = buttonWidth,
                 height = 24.dp
             )
             .clickable(
@@ -111,15 +115,31 @@ fun JoinButton(onClick: (Boolean) -> Unit = {}){
                             onClick.invoke(false)
                             JoinButtonState.IDLE
                         }
-                }),
+                }
+            ),
         contentAlignment = Alignment.Center
     ) {
-        Icon(
-            imageVector = iconAsset,
-            contentDescription = "Plus Icon",
-            tint = iconTintColor,
-            modifier = Modifier.size(16.dp)
-        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = iconAsset,
+                contentDescription = "Plus Icon",
+                tint = iconTintColor,
+                modifier = Modifier.size(16.dp)
+            )
+            Text(
+                text="Join",
+                color = Color.White,
+                fontSize = 14.sp,
+                maxLines = 1,
+                modifier = Modifier
+                    .widthIn(
+                        min = 0.dp,
+                        max = textMaxWidth
+                    )
+            )
+        }
     }
 }
 
